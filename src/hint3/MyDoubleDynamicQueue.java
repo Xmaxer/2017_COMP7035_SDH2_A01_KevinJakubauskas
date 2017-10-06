fd502ab54e88ce5cf0aefefd18eb1b75cde9aae6
Kevin <7211424+Xmaxer@users.noreply.github.com>
2017-10-06 19:28:43 +0100

    Finished 'hint3'

--- src/hint3/MyDoubleDynamicQueue.java
+++ src/hint3/MyDoubleDynamicQueue.java
@@ -6,8 +6,9 @@ public class MyDoubleDynamicQueue<T> implements MyQueue<T> {
 	//--------------------------------------------------
 	// Attributes
 	//--------------------------------------------------
+	private MyDoubleLinkedNode<T> current = null;
+	private MyDoubleLinkedNode<T> last = null;
 
-	
 	//-------------------------------------------------------------------
 	// Basic Operation --> Check if myQueue is empty: myCreateEmpty
 	//-------------------------------------------------------------------	
@@ -20,13 +21,20 @@ public class MyDoubleDynamicQueue<T> implements MyQueue<T> {
 	//-------------------------------------------------------------------	
 	public boolean isEmpty(){
 
+		if(current == null)
+		{
+			return true;
+		}
+
+		return false;
 	}
-	
+
 	//-------------------------------------------------------------------
 	// Basic Operation (Partial) --> Get first element from front of MyQueue: first
 	//-------------------------------------------------------------------
 	public T first(){
 
+		return current.getInfo();
 	}
 
 	//-------------------------------------------------------------------
@@ -34,19 +42,68 @@ public class MyDoubleDynamicQueue<T> implements MyQueue<T> {
 	//-------------------------------------------------------------------
 	public void addByFirst(T element){
 
+		if(current == null)
+		{
+			current = new MyDoubleLinkedNode<T>(null ,element, null);
+			last = current;
+		}
+		else
+		{
+			MyDoubleLinkedNode<T> newNode = new MyDoubleLinkedNode<T>(null , element, current);
+			current.setLeft(newNode);
+			current = newNode;
+		}
 	}
-	
+
 	//-------------------------------------------------------------------
 	// Basic Operation (Partial) --> Remove element from front of MyQueue: removeByFirst 
 	//-------------------------------------------------------------------	
 	public void removeByFirst(){
 
+		if(current != null)
+		{
+			current = current.getRight();
+			
+			if(current != null)
+			{
+				current.setLeft(null);
+			}
+			/*if(current.getRight() != null)
+			{
+				current.getRight().setLeft(null);
+				current = current.getRight();
+			}
+			else
+			{
+				current = null;
+				old = null;
+			}*/
+		}
+		else
+		{
+			System.out.println("ERROR: List is empty.");
+		}
 	}
 
 	//-------------------------------------------------------------------
 	// Basic Operation (Partial) --> Get first element from front of MyQueue: last
 	//-------------------------------------------------------------------
 	public T last(){
+
+		if(current == null)
+		{
+			System.out.println("ERROR: List is empty.");
+			return null;
+		}
+		MyDoubleLinkedNode<T> next = current.getRight();
+		MyDoubleLinkedNode<T> prev = current;
+		while(next != null)
+		{
+			prev = next;
+			next = next.getRight();
+		}
+
+		return prev.getInfo();
 
 	}
 
@@ -55,13 +112,35 @@ public class MyDoubleDynamicQueue<T> implements MyQueue<T> {
 	//-------------------------------------------------------------------
 	public void addByLast(T element){
 
+		if(current == null)
+		{
+			current = new MyDoubleLinkedNode<T>(null, element, null);
+			last = current;
+		}
+		else
+		{
+			MyDoubleLinkedNode<T> newNode = new MyDoubleLinkedNode<T>(last, element, null);
+			last.setRight(newNode);
+			last = newNode;
+		}
 	}
-	
+
 	//-------------------------------------------------------------------
 	// Basic Operation (Partial) --> Remove element from front of MyQueue: removeByFirst 
 	//-------------------------------------------------------------------	
 	public void removeByLast(){
 
+		if(current == null)
+		{
+			System.out.println("ERROR: List is empty.");
+			return;
+		}
+		
+		last = last.getLeft();
+		
+		if(last != null)
+		{
+			last.setRight(null);
+		}	
 	}
-	
 }
